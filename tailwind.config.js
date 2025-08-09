7bc75affa58a6c9ad7031103197689f268a3e138
SaiDhinakar <saidhinakars899@gmail.com>
2025-08-09 10:18:12 +0530

    base styling applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     './Authentication/templates/**/*.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'accent': 'var(--accent)',
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'background': 'var(--background)',
+        'section-bg': 'var(--section-bg)',
+        'text-primary': 'var(--text-primary)',
+        'text-secondary': 'var(--text-secondary)',
+        'button-primary': 'var(--button-primary)',
+        'button-secondary': 'var(--button-secondary)',
+      }
+    },
   },
   plugins: [],
 }
